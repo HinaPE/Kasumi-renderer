8bc4b7e6d3b190cec1496c5aab1f1330868cf5dd
Xayah-Hina <ycxiang14sep@gmail.com>
2022-11-18 03:29:22 +0800

    add menu

--- workbench/gui/manager.cpp
+++ workbench/gui/manager.cpp
@@ -1,4 +1,6 @@
 #include "manager.h"
+#include <imgui/imgui.h>
+#include <iostream>
 
 Kasumi::Workbench::Manager::Manager()
 {
@@ -7,6 +9,35 @@ Kasumi::Workbench::Manager::Manager()
 
 void Kasumi::Workbench::Manager::render()
 {
+    if (ImGui::BeginMainMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            if (ImGui::MenuItem("Open Scene (Ctrl+o)"))
+                std::cout << std::endl;
+            if (ImGui::MenuItem("Export Scene (Ctrl+e)"))
+                std::cout << std::endl;
+            if (ImGui::MenuItem("Save Scene (Ctrl+s)"))
+                std::cout << std::endl;
+            ImGui::EndMenu();
+        }
+
+        if (ImGui::BeginMenu("Edit"))
+        {
+            if (ImGui::MenuItem("Undo (Ctrl+z)"))
+                std::cout << std::endl;
+            if (ImGui::MenuItem("Redo (Ctrl+y)"))
+                std::cout << std::endl;
+            if (ImGui::MenuItem("Edit Debug Data (Ctrl+d)"))
+                std::cout << std::endl;
+            if (ImGui::MenuItem("Settings"))
+                std::cout << std::endl;
+            ImGui::EndMenu();
+        }
+
+        ImGui::Text("FPS: %.0f", ImGui::GetIO().Framerate);
+        ImGui::EndMainMenuBar();
+    }
     for (auto &gui: _gui)
         gui.second->render();
 }
--- workbench/gui/simulate.cpp
+++ workbench/gui/simulate.cpp
@@ -1,12 +1,11 @@
 #include "simulate.h"
-#include <iostream>
+#include <imgui/imgui.h>
+
 void Kasumi::Workbench::Simulate::render()
 {
-
+    ImGui::Begin("Simulate");
+    ImGui::Text("Hello, world!");
+    ImGui::End();
 }
 
-void Kasumi::Workbench::Simulate::event(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        std::cout << "A" << std::endl;
-}
+void Kasumi::Workbench::Simulate::event(GLFWwindow *window) {}
