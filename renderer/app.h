8d1e725a35eba1c883721b5abe90ee853ac7c2a3
Xayah-Hina <ycxiang14sep@gmail.com>
2022-11-25 21:09:23 +0800

    add simulation button

--- renderer/app.h
+++ renderer/app.h
@@ -18,38 +18,44 @@ namespace Kasumi
 class Renderer : public Kasumi::App
 {
 public:
-    void load_api(const Kasumi::ApiPtr &api);
-    void prepare() final;
-    void update(double dt) final;
-    auto quit() -> bool final;
-
-private:
-    friend class Kasumi::Platform;
-    void key(int key, int scancode, int action, int mods) final;
-    void mouse_button(int button, int action, int mods) final;
-    void mouse_scroll(double x_offset, double y_offset) final;
-    void mouse_cursor(double x_pos, double y_pos) final;
-
-private:
-    void ui_menu();
-    void ui_sidebar();
-    void reset_state();
-    float _next_x = 0.f, _next_y = 0.f;
-
-private:
-    ManagerPtr _manager;
-    ScenePtr _scene;
-    UndoPtr _undo;
-    std::vector<Kasumi::ApiPtr> _apis;
-    FramebufferPtr _framebuffer; // NOT COMPLETED YET
+	struct Opt
+	{
+		bool api_running = false;
+	} _opt;
 
 public:
-    explicit Renderer(std::string scene);
-    Renderer(const Renderer &) = delete;
-    Renderer(Renderer &&) = delete;
-    ~Renderer() = default;
-    auto operator=(const Renderer &) -> Renderer & = delete;
-    auto operator=(Renderer &&) -> Renderer & = delete;
+	void load_api(const Kasumi::ApiPtr &api);
+	void prepare() final;
+	void update(double dt) final;
+	auto quit() -> bool final;
+
+private:
+	friend class Kasumi::Platform;
+	void key(int key, int scancode, int action, int mods) final;
+	void mouse_button(int button, int action, int mods) final;
+	void mouse_scroll(double x_offset, double y_offset) final;
+	void mouse_cursor(double x_pos, double y_pos) final;
+
+private:
+	void ui_menu();
+	void ui_sidebar();
+	void reset_state();
+	float _next_x = 0.f, _next_y = 0.f;
+
+private:
+	ManagerPtr _manager;
+	ScenePtr _scene;
+	UndoPtr _undo;
+	std::vector<Kasumi::ApiPtr> _apis;
+	FramebufferPtr _framebuffer; // NOT COMPLETED YET
+
+public:
+	explicit Renderer(std::string scene);
+	Renderer(const Renderer &) = delete;
+	Renderer(Renderer &&) = delete;
+	~Renderer() = default;
+	auto operator=(const Renderer &) -> Renderer & = delete;
+	auto operator=(Renderer &&) -> Renderer & = delete;
 };
 }
 
