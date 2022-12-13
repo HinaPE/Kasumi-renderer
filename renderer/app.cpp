e3f2f58593b766039d501154ca02180792b3d2ed
Xayah-Hina <ycxiang14sep@gmail.com>
2022-12-13 17:55:33 +0800

    update comments

--- renderer/app.cpp
+++ renderer/app.cpp
@@ -3,13 +3,11 @@
 #include "GLFW/glfw3.h"
 #include "nfd/nfd.h"
 #include "imgui/imgui.h"
+
 #include <utility>
 
 Kasumi::Renderer::Renderer(std::string scene_file, int width, int height, const std::string &title)
-		: App(width, height, title), _scene_file(std::move(scene_file)), _scene(std::move(std::make_shared<Scene>())), _manager(std::move(std::make_shared<Manager>())), _debug_frame(std::make_shared<Framebuffer>(width, height, 0.4, 0.2, 1.0, 1.0)), _apis()
-{
-	_debug_frame->render_callback = [&]() {};
-}
+		: App(width, height, title), _scene_file(std::move(scene_file)), _scene(std::move(std::make_shared<Scene>())), _manager(std::move(std::make_shared<Manager>())), _debug_frame(std::make_shared<Framebuffer>(width, height, 0.4, 0.2, 1.0, 1.0)), _apis() { _debug_frame->render_callback = [&]() {}; }
 
 auto Kasumi::Renderer::load_api(const Kasumi::ApiPtr &api) -> std::shared_ptr<Kasumi::App>
 {
@@ -20,21 +18,29 @@ auto Kasumi::Renderer::load_api(const Kasumi::ApiPtr &api) -> std::shared_ptr<Ka
 void Kasumi::Renderer::prepare() { _scene->read_scene(std::string(SceneDir) + _scene_file); }
 void Kasumi::Renderer::update(double dt)
 {
+	// update plugin api scene (physics scene)
 	if (_opt.api_running)
 		for (auto &api: _apis)
 			api->step(dt);
+
+	// render ui
 	reset_state();
 	ui_menu();
 	ui_sidebar();
 	_manager->render(_scene, _next_x, _next_y);
+
+	// first draw call: render the world to the main window
 	_scene->render();
 
+	// second draw call: render the world to the debug framebuffer
 	_debug_frame->use();
-	for (auto &o:_scene->_scene_objects)
+	for (auto &o: _scene->_scene_objects)
 		o.second->framebuffer_mode(true);
 	_scene->render();
-	for (auto &o:_scene->_scene_objects)
+	for (auto &o: _scene->_scene_objects)
 		o.second->framebuffer_mode(false);
+
+	// third draw call: render the debug framebuffer to the main window
 	_debug_frame->unuse();
 	_debug_frame->render();
 }
