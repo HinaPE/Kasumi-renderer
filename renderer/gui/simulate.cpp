6a8c80c43dbb4422b6fca0b9be4d58d1039e0d42
Xayah-Hina <ycxiang14sep@gmail.com>
2022-12-17 04:40:54 +0800

    update math plotter

--- renderer/gui/simulate.cpp
+++ renderer/gui/simulate.cpp
@@ -1,19 +1,21 @@
 #include "simulate.h"
 #include "imgui.h"
 
-Kasumi::Simulate::Simulate(int width, int height) : _debug_frame(std::make_shared<Framebuffer>(width, height, 0.4, 0.2, 1.0, 1.0)) { _debug_frame->render_callback = [&]() {}; }
+Kasumi::Simulate::Simulate(int width, int height) : _debug_frame(std::make_shared<Framebuffer>(width, height, 0.4, 0.2, 1.0, 1.0))
+{
+	_debug_frame->render_callback = [&]()
+	{
+		// draw call: render the world to the debug framebuffer
+		for (auto &o: _scene->_scene_objects)
+			o.second->framebuffer_mode(true);
+		_scene->render();
+		for (auto &o: _scene->_scene_objects)
+			o.second->framebuffer_mode(false);
+	};
+}
 
 void Kasumi::Simulate::render()
 {
-	// draw call: render the world to the debug framebuffer
-	_debug_frame->use();
-	for (auto &o: _scene->_scene_objects)
-		o.second->framebuffer_mode(true);
-	_scene->render();
-	for (auto &o: _scene->_scene_objects)
-		o.second->framebuffer_mode(false);
-
-	// draw call: render the debug framebuffer to the main window
-	_debug_frame->unuse();
+	// draw call: render the debug framebuffer onto the main window
 	_debug_frame->render();
 }
