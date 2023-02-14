b7ff58b581fb2d2c03acf57a51d64d2dd6de1b48
Xayah-Hina <ycxiang14sep@gmail.com>
2023-02-15 02:30:38 +0800

    renderer3D

---
+++ renderer3D/app.h
@@ -0,0 +1,16 @@
+#ifndef HINAPE_APP_H
+#define HINAPE_APP_H
+
+#include "platform.h"
+
+#include "camera.h"
+#include "mesh.h"
+
+namespace Kasumi
+{
+class Renderer3D : public App
+{
+};
+}
+
+#endif //HINAPE_APP_H
