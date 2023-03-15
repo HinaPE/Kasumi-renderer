24055d02ecf772f73457d3828cd008d8c08ca098
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-15 18:10:36 +0800

    [tutorial] shader painter

---
+++ shader_painter/shader_painter.h
@@ -0,0 +1,12 @@
+#ifndef HINAPE_SHADER_PAINTER_H
+#define HINAPE_SHADER_PAINTER_H
+
+// @formatter:off
+namespace Kasumi
+{
+class ShaderPainter
+{
+};
+} // namespace Kasumi
+
+#endif //HINAPE_SHADER_PAINTER_H
