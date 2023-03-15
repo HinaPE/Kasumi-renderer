062389cb31fc9632ab4a943d22b3f51bbd575511
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-15 13:57:24 +0800

    [screen space fluid] volume renderer

---
+++ renderer3D/volume_renderer.h
@@ -0,0 +1,17 @@
+#ifndef HINAPE_VOLUME_RENDERER_H
+#define HINAPE_VOLUME_RENDERER_H
+
+#include "GLFW/glfw3.h"
+#include "scene.h"
+
+// @formatter:off
+
+namespace Kasumi
+{
+class VolumeRenderer : public App
+{
+	
+};
+} // namespace HinaPE
+
+#endif //HINAPE_VOLUME_RENDERER_H
